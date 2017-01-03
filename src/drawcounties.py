5b501ed2014eb12bd5dc0c4be62a644a367346c7
Annak17 <annak17@lakesideschool.org>
2017-01-03 12:04:18 -0800

    Added 2012 results

--- src/drawcounties.py
+++ src/drawcounties.py
@@ -1,2 +1,8 @@
-#This file will draw a county given an input color, and county name
-#(we are assuming Georgia for everything)
+#This file will become a function that draws a county given an input color, and
+#county name (assuming Georgia for everything)
+#Currently, it will do a lot of extra stuff that will be unnecessary later
+
+import numpy as np
+import matplotlib as plot
+f = open('data/georgia_county_locations.txt', 'r')
+print (f)
