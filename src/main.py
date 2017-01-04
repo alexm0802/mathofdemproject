8277e937f6bfb76fc6a4cf0f3f04d28977a4583e
Annak17 <annak17@lakesideschool.org>
2017-01-04 09:12:16 -0800

    Made main better

--- src/main.py
+++ src/main.py
@@ -11,11 +11,28 @@ combining maps
 calculating scores (whatever we choose for this to mean)
 """
 #a map will be  ____ = [14][] (or possibly shorter if separating large counties)
-
-def random_map ():
+"""
+def random_map (county_data):
 
 def comb_map (map1, map2):
 
-def calc_score (map):
+def calc_score (map):"""
 
 def main ():
+    f = open('data/georgia_2012_results.csv', 'r')
+    f.readline()
+    county_data = [] #county_data[0]=names, county_data[1]=total votes
+                    #county_data[2]=rep votes, county_data[4]= dem votes
+    for line in f:
+        temp = line.split(',')
+        county_data.append([temp[2], temp[7], temp[4], temp[5]])
+    maps = []
+    scores = []
+    for i in range(0, 10):
+        maps.append(random_map(county_data))
+    for j in maps:
+        scores.append(calc_score(maps[j]))
+    while (True):
+
+
+if __name__ == "__main__": main()
