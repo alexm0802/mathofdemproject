8ace4e30ca9e8389535573f048512db27ea99e2a
Annak17 <annak17@lakesideschool.org>
2017-01-03 21:28:47 -0800

    Created main file

---
+++ src/main.py
@@ -0,0 +1,21 @@
+"""
+initiate everything, and create arrays of counties, population, and %rep
+deal with the counties that are super big, and create a few districts...
+create 5 random district combinations, and calculate scores for them
+combine best 2 district combinations into 1 map
+mutate that map into 5 ones, and repeat previous three steps
+
+issues:
+creating the random maps: making sure population matches
+combining maps
+calculating scores (whatever we choose for this to mean)
+"""
+#a map will be  ____ = [14][] (or possibly shorter if separating large counties)
+
+def random_map ():
+
+def comb_map (map1, map2):
+
+def calc_score (map):
+
+def main ():
